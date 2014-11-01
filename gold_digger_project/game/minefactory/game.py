b573ef2d9785d89edb27c8b33aedfd0a56b31d33
Sean Tomlinson <seantomlinson@outlook.com>
2014-11-01 20:20:03 +0000

    Gold-Digger command-line game now running, changes to be made to encapsulate the game entirely within the game class

--- gold_digger_project/game/minefactory/game.py
+++ gold_digger_project/game/minefactory/game.py
@@ -1,17 +1,74 @@
 from minefactory import *
+from yieldgenerator import *
+from cuegenerator import *
 
-class Game():
+# class to represent an instance of a game
 
-    def __init__(self, mine_list, current_mine):
-        self.mine_list = mine_list
-        self.current_mine = current_mine
 
-    
-    def start_game():
-        pass
+class Game:
 
-    def check_end():
-        pass
+    def __init__(self, time_remaining, no_mines, depth):
+        self.mine_list = []
+        self.mine_position = 0
+        self.player_gold = 0
+        self.time_remaining = time_remaining
+        self.no_mines = no_mines
+        self.depth = depth
+        self.current_mine = None
+        self.current_block = None
 
-    def get_state():
-        pass
+    # function to create a mine factory and construct a game consisting of several mines
+    def start_game(self, max_yield, yield_generator, cue_generator, scan_accuracy):
+        mine_factory = MineFactory(yield_generator, cue_generator)
+        for i in range(self.no_mines):
+            new_mine = mine_factory.create_mine(self.depth, max_yield, scan_accuracy)
+            self.mine_list.append(new_mine)
+        self.current_mine = self.mine_list[self.mine_position]
+        self.current_block = self.current_mine.get_current_block()
+
+    # function to return the current state of the game
+    def get_state(self):
+        state_dict = {"mine_pos": self.mine_position + 1,
+                      "block_pos": self.current_mine.get_block_position(),
+                      "player_gold": self.player_gold,
+                      "time_remaining": self.time_remaining,
+                      "gold_cue": self.current_block.get_cue()}
+        return state_dict
+
+    # function to return the players current gold total
+    def get_player_gold(self):
+        return self.player_gold
+
+    # function to check for no time remaining and the game being over
+    def check_end(self):
+        if self.time_remaining != 0:
+            return 0
+
+    # function to check the player has enough time to perform the action they have selected
+    def check_enough_time(self, action_cost):
+        if action_cost < self.time_remaining:
+            return 1
+        else:
+            return 0
+
+    # function to check if the player has reached the last mine in the mine list
+    def mine_list_exhausted(self):
+        if self.mine_position == self.no_mines - 1:
+            return 1
+        else:
+            return 0
+
+    # function that digs a single block and adds the gold found to the players total
+    def player_dig(self, dig_cost):
+        gold_collected = self.current_block.get_yield()
+        self.current_block = self.current_mine.get_next_block()
+        self.player_gold += gold_collected
+        self.time_remaining -= dig_cost
+        return gold_collected
+
+    # function that moves the player to the next available mine and puts them back above ground (block 0)
+    def player_move(self, move_cost):
+            self.mine_position += 1
+            self.current_mine = self.mine_list[self.mine_position]
+            self.current_block = self.current_mine.get_current_block()
+            self.time_remaining -= move_cost
--- gold_digger_project/game/minefactory/main.py
+++ gold_digger_project/game/minefactory/main.py
@@ -1,9 +1,70 @@
-__author__ = 'Laura'
+from game import *
+from yieldgenerator import *
+from cuegenerator import *
+from time import sleep
 
-#Test harness for Mine Factory
+time_remaining = 200                                    # the player starts with 300 units of time
+no_mines = 10                                           # the game will consist of ten individual mines
+depth = 10                                              # each mine will be 10 blocks deep
+max_yield = 100                                         # the player has the chance to mine a maximum of 100 gold
+scan_accuracy = 0.6                                     # the player's equipment has a predetermined accuracy
+dig_cost = 10                                           # the amount of time it takes to dig
+move_cost = 60                                          # the amount of time it takes to move to another mine
+yield_generator = RandomYield(depth, max_yield)
+cue_generator = AccurateCue(max_yield, scan_accuracy)
 
-from minefactory import *
+game = Game(time_remaining, no_mines, depth)
+game.start_game(max_yield, yield_generator, cue_generator, scan_accuracy)
 
-factory = MineFactory(10)
-print factory.make_mine("random", "random")
+print "\nWelcome to Gold Digger! Try to collect as much gold as you can within the time limit!\n"
+
+while game.check_end() == 0:
+
+    valid_move = False
+
+    game_state = game.get_state()
+    print "You are currently located at mine {} of {}, block {} of {}. You are carrying {} gold and have {} " \
+          "units of time remaining. Your scanning equipment suggests that there are {} gold pieces to " \
+          "be uncovered here.\n".format(game_state.get("mine_pos"), no_mines, game_state.get("block_pos"),
+                                        depth, game_state.get("player_gold"), game_state.get("time_remaining"),
+                                        game_state.get("gold_cue"))
+
+    print "Do you choose to DIG further into the mine (press D on your keyboard, cost: 20 units)" \
+          " or MOVE to a new location in search of another mine (press M on your keyboard, cost: 40 units)?\n"
+
+    while valid_move == 0:
+        player_choice = raw_input("-->")
+
+        while player_choice not in ["d", "m", "D", "M"]:
+            print "\nInvalid choice, please choose to DIG (D) or MOVE (M).\n"
+            player_choice = raw_input("-->")
+
+        if player_choice == "d":
+            gold_collected = game.player_dig(dig_cost)
+            print "\nDigging...\n"
+            sleep(2)
+            print "You uncover", gold_collected, "gold pieces!\n"
+            valid_move = 1
+
+            if game.current_mine.mine_exhausted() == 1:
+                print "You have exhausted this mine of its resources and must move on.\n"
+                print "You go off in search of your fortune elsewhere...\n"
+                game.player_move(move_cost)
+                sleep(3)
+        else:
+            if game.mine_list_exhausted() == 1:
+                print "\nYou have already explored all of the mines in this area, keep digging!\n"
+            elif game.check_enough_time(move_cost) == 0:
+                print "\nThere's not enough time left in the day to go in search of a new mine!\n"
+            else:
+                print "\nYou go off in search of your fortune elsewhere...\n"
+                game.player_move(move_cost)
+                sleep(3)
+                valid_move = 1
+
+print "Time up! You managed to mine a grand total of ", game.get_player_gold(), " gold pieces!\n\nGame Over!"
+
+
+
+
 
