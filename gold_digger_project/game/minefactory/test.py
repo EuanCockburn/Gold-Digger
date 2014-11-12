d0ec6b83d347e38bb59ed084037a5fe73e0ba4a8
Laura <ellejay@users.noreply.github.com>
2014-11-12 21:00:42 +0000

    First implementation of optimal yield algorithm, to be integrated into the game class files

--- gold_digger_project/game/minefactory/test.py
+++ gold_digger_project/game/minefactory/test.py
@@ -1,14 +1,35 @@
 from yieldgenerator import *
 import random
 
+
+def get_optimal(yield_array, dig_cost, move_cost):
+        x = move_cost
+        y = 0
+        max_m_pos = -1
+        max_m = -100
+
+        for i in range(0, len(yield_array)):
+            y += yield_array[i]
+            x += dig_cost
+            m = y / x
+            if m > max_m:
+                max_m = m
+                max_m_pos = i
+
+        return max_m_pos
+
 depth = 10
 max_yield = 75
 slope = 6
+dig = 5
+move = 20
 
-print "Random", RandomYield(depth, max_yield).generate_array()
+rand = RandomYield(depth, max_yield).generate_array()
+print "Random", rand, get_optimal(rand, dig, move)
 print "Constant", ConstantYield(depth, max_yield).generate_array()
 print "Linear", LinearYield(depth, max_yield).generate_array(20)
-print "Quadratic", QuadraticYield(depth, max_yield).generate_array(2, 5)
+quad = QuadraticYield(depth, max_yield).generate_array(2, 5)
+print "Quadratic", quad, get_optimal(quad, dig, move)
 print "Exponential", ExponentialYield(depth, max_yield).generate_array(2.5, 4)
 print "Cubic", CubicYield(depth, max_yield).generate_array(0.5)
 
@@ -22,11 +43,14 @@ print "Yukon", QuadraticYield(depth, k).generate_array(1, 0)
 span = [10, 12, 15, 20]
 print "Brazil", QuadraticYield(depth, random.choice(span)).generate_array(-1.5, 5)
 
-print "Scotland", QuadraticYield(depth, random.randint(-10, 5) + 90).generate_array(0.7, random.randint(-8, -2))
+scot = QuadraticYield(depth, random.randint(-10, 5) + 90).generate_array(0.7, random.randint(-8, -2))
+print "Scotland", scot, get_optimal(scot, dig, move)
 
 span = [0.2, 0.1, 0.3, 6, 8]
-print "South Africa", QuadraticYield(depth, 50 + random.randint(-3, 3)).generate_array(random.choice(span), 0)
+southa = QuadraticYield(depth, 50 + random.randint(-3, 3)).generate_array(random.choice(span), 0)
+print "South Africa", southa, get_optimal(southa, dig, move)
 
 span = [10, -20]
-print "Victoria", QuadraticYield(depth, random.randint(60, 100) + random.choice(span)).generate_array(1, 3)
+victoria = QuadraticYield(depth, random.randint(60, 100) + random.choice(span)).generate_array(1, 3)
+print "Victoria", victoria, get_optimal(victoria, dig, move)
 
